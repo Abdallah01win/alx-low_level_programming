7a2fe5efe897eabfb781f565845cacd18bf90339
Abdallah01win <abdellah01win@gmail.com>
2023-06-22 15:31:41 -0700

    first task attempt

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+    int num = 0;
+
+    printf("Testing positive_or_negative() function with 0:\n");
+    positive_or_negative(num);
+
+    return 0;
+}
