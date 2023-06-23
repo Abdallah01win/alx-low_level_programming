e9c531c63059301fa9e007f035d3ed4f004aab55
Abdallah01win <abdellah01win@gmail.com>
2023-06-23 15:17:55 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code for more number function.
  *
  * Return: Always 0.
  */
-int main(void)
+int more_numbers(void)
 {
-	more_numbers();
-	return (0);
+  for(int i = 0; i > 10; i++)
+    {
+      for(int j = 0; j > 14; j++)
+	{
+	  printf("%d", j);
+	}
+      printf("\n");
+    }
+  return(0);
 }
