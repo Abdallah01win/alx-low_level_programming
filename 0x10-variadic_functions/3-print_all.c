49d5b72a65423891156df7cc2e812a8b38c27e66
Abdallah01win <abdellah01win@gmail.com>
2023-07-20 04:14:24 -0700

    fix task3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,91 +1,94 @@
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "variadic_functions.h"
-/**
- * print_c - prints char
- * @a: list to give
- * Return: always 0
- */
-int print_c(va_list a)
-{
-	printf("%c", va_arg(a, int));
-	return (0);
-}
-/**
- * print_i - prints int
- * @a: list to give
- * Return: always 0
- */
-int print_i(va_list a)
-{
-	printf("%d", va_arg(a, int));
-	return (0);
-}
-/**
- * print_f - prints float
- * @a: list to give
- * Return: always 0
- */
-int print_f(va_list a)
-{
-	printf("%f", va_arg(a, double));
-	return (0);
-}
-/**
- * print_s - prints string
- * @a: list to give
- * Return: always 0
- */
-int print_s(va_list a)
-{
-	char *s;
+#include <stdio.h>
 
-	s = va_arg(a, char *);
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return (0);
-	}
-	printf("%s", s);
-	return (0);
-}
 /**
- * print_all - prints all
- * @format: format string that says arg types
+ * print_char - function that prints the char type
+ * @arguments: arguments from va_list
  *
  */
+
+void print_char(va_list arguments)
+{
+	printf("%c", va_arg(arguments, int));
+}
+
+/**
+ * print_int - function that prints the integer type
+ * @arguments: arguments from va_list
+ *
+ */
+
+void print_int(va_list arguments)
+{
+	printf("%d", va_arg(arguments, int));
+}
+
+/**
+ * print_float - function that prints the float type
+ * @arguments: arguments from va_list
+ *
+ */
+
+void print_float(va_list arguments)
+{
+	printf("%f", va_arg(arguments, double));
+}
+
+/**
+ * print_string - function that prints the string type
+ * @arguments: arguments from va_list
+ *
+ */
+
+void print_string(va_list arguments)
+{
+	char *p;
+
+	p = (va_arg(arguments, char *));
+	if (p == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", p);
+}
+
+/**
+ * print_all - function that prints all type the arguments
+ * @format: first value
+ *
+ */
+
 void print_all(const char * const format, ...)
 {
-	int i, j;
-	char *sep = "";
-	char *sep2 = ", ";
-	va_list anyArgs;
-	printer ops[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"s", print_s},
-		{"f", print_f},
+	va_list arguments;
+	typeprint_t types[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
 		{NULL, NULL}
 	};
+	int i = 0, j = 0, a = 0;
 
-	va_start(anyArgs, format);
-	i = 0;
-	while (format != NULL && format[i])
+	va_start(arguments, format);
+	while (format && format[j])
 	{
-		j = 0;
-		while (ops[j].f != NULL)
+		printf(a == 1 ? ", " : "");
+		a = 0;
+		while (types[i].type_p)
 		{
-			if (format[i] == *(ops[j].c))
+			if (*(types[i].type_p) == format[j])
 			{
-				printf("%s", sep);
-				ops[j].f(anyArgs);
+			types[i].function(arguments);
+			a = 1;
 			}
-			j++;
+			i++;
 		}
-		sep = sep2;
-		i++;
+		i = 0;
+		j++;
 	}
+	va_end(arguments);
 	printf("\n");
-	va_end(anyArgs);
 }
+
