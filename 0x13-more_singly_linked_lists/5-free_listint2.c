6c0ce49d8874fd25f3c694d79383a9bd944b3e95
Abdallah01win <abdellah01win@gmail.com>
2023-07-31 13:14:46 -0700

    restore 5 and add 11 & 12

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,20 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- * free_listint2 - frees a list but head is null
- * @head: linked list and head
+ * free_listint2 - frees a linked list
+ * @head: pointer to a linked list
  *
  */
+
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
-
 	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
-		tmp = *head;
 		free(*head);
-		*head = tmp->next;
+		*head = (*head)->next;
 	}
-	*head = NULL;
+	free(*head);
+	head = NULL;
 }
