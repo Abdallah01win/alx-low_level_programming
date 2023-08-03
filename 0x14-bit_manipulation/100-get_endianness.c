777ed74c6b7d2013514794a47cef8ac0c434f93b
Abdallah01win <abdellah01win@gmail.com>
2023-08-03 13:57:13 -0700

    endianness task 3

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
 #include "main.h"
 
-int get_endianness(void) {
-    int value;
-    char *byte;
+/**
+ * get_endianness - checks if multibyte data is stored as little or big endian
+ * Return: first value stored, 0 if big endian, 1 if little
+ */
 
-    value = 0x01;
-    byte = (char *)&value;
+int get_endianness(void)
+{
+	/* have a 4 byte int hold some hex number 0x00000001 */
+	/* test if first byte stored is 00 or 01 */
+	/* big endian would store 00, little would store 01 */
 
-    return (*byte == 0x01) ? 1 : 0;
+	int n = 0x00000001;
+	char *c = (char *)&n;
+
+	return (c[0]);
 }
